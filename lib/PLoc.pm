9015ead9bd52277fae7b84d73422be74b636d269
Helmut Wollmersdorfer <helmut@wollmersdorfer.at>
2011-12-23 23:08:18 +0100

    defined methods

---
+++ lib/PLoc.pm
@@ -0,0 +1,43 @@
+package PLoc;
+
+use strict;
+use warnings;
+
+sub new {}
+
+# locale
+sub get_locale {}
+
+sub get_locale_normalized {}
+
+sub get_locale_effective {}
+
+sub parse_locale {}
+
+# languages
+sub language_from_code {}
+
+sub code_from_language {}
+
+sub language_native {}
+
+# territories
+sub territory_from_code {}
+
+sub code_from_territory {}
+
+# decimals
+
+sub decimal {}
+
+sub number_group {}
+
+sub format_number {}
+
+# DateTime
+
+# plural forms
+
+sub get_plural_categories {}
+
+sub select_plural_form {}
